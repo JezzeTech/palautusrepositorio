da31ee85f390d8318a41174527c63ad1a6da014f
Jesse Raatikainen <jesse.v.raatikainen@student.jyu.fi>
2025-11-05 21:08:31 +0200

    Toml-tiedoston_lukeminen_ja_formatointi

--- osa2/project-reader/src/project_reader.py
+++ osa2/project-reader/src/project_reader.py
@@ -1,6 +1,6 @@
+import dom_toml
 from urllib import request
 from project import Project
-
 
 class ProjectReader:
     def __init__(self, url):
@@ -9,7 +9,13 @@ class ProjectReader:
     def get_project(self):
         # tiedoston merkkijonomuotoinen sisältö
         content = request.urlopen(self._url).read().decode("utf-8")
-        print(content)
 
+        data = dom_toml.loads(content)
+        name = data["tool"]["poetry"]["name"]
+        description = data["tool"]["poetry"]["description"]
+        license = data["tool"]["poetry"]["license"]
+        authors = data["tool"]["poetry"]["authors"]
+        dependencies = data["tool"]["poetry"]["dependencies"]
+        dev_dependencies = data["tool"]["poetry"]["group"]["dev"]["dependencies"]
         # deserialisoi TOML-formaatissa oleva merkkijono ja muodosta Project-olio sen tietojen perusteella
-        return Project("Test name", "Test description", [], [])
+        return Project(name, description, license, authors, dependencies, dev_dependencies)
